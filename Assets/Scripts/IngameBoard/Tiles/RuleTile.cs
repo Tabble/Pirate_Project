c5bc36a5750300e1de92a4c0494b5df841e4c7c8
Tabble <bluecher.tanja@gmail.com>
2018-07-28 10:35:54 +0200

    rule tile test mit custom editor hinzugefügt.

---
+++ Assets/Scripts/IngameBoard/Tiles/RuleTile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class RuleTile {
+
+    [SerializeField]
+    public NeighborStatus[] Neighbors = new NeighborStatus[8];
+    public Sprite Sprite;
+    public Material Material;
+
+}
---
+++ Assets/Scripts/IngameBoard/Tiles/RuleTileTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RuleTileTest : MonoBehaviour {
+
+    public List<RuleTile> RuleTiles;
+   
+    public RuleTileTest()
+    {
+        RuleTiles = new List<RuleTile>();
+        RuleTiles.Add(new RuleTile());
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+[System.Serializable]
+public enum NeighborStatus
+{
+    egal,
+    no,
+    same
+}
